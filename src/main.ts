6091201fc0eb75e2c6ed16fd994836fc973f5df3
JRM_MucHyp <jana.ruiz.morell@muenchenerhyp.com>
2021-07-27 19:43:25 +0200

    Init prettier config file, settings for devtools to work

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "tailwindcss/tailwind.css";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'tailwindcss/tailwind.css';
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+// Set config performance to true in order to allow Vue DevTools to show in browwer
+app.config.performance = true;
+app.use(store).use(router).mount('#app');
